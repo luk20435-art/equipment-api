6a006edfd28e4c1528cabe91d306e317addf5f29
lukman <luk20435@gmail.com>
2026-05-13 13:46:33 +0700

    fix: move enableCors before helmet, disable crossOriginResourcePolicy

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,20 @@ import * as bodyParser from 'body-parser';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  app.use(helmet());
+  const app = await NestFactory.create(AppModule, { cors: false });
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  app.use(helmet({ crossOriginResourcePolicy: false }));
 
   // Increase request body size limit
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  
-  // Enable CORS for frontend
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
 
   // Enable validation
   app.useGlobalPipes(
